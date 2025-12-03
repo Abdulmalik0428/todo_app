0550e9f43585b2a1551fec246202866a03fcd2ce
Abdulmalik0428 <158091874+Abdulmalik0428@users.noreply.github.com>
2025-12-04 02:18:59 +0500

    Update settings.py

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-lp%uotlq0z^y5k8+3(%nqyu-6o2njrvsgbbnxndz78lwxn!3z#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['185.123.45.67']
 
 
 # Application definition
